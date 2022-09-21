b50f58e1454a627f517f8977d8a38ac18ba50f44
prakaasnike <erprakashnayak@gmail.com>
2022-09-21 20:34:56 +0545

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="mx-auto items-center justify-center">
+    <div
+      class="
+        flex
+        mx-auto
+        justify-center
+        h-[40vh]
+        items-center
+        bg-black
+        space-x-4
+      "
+    >
+      <span>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="h-16 p-2" alt="Vite logo" /> </a
+      ></span>
+      <span
+        ><a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="h-16 p-2" alt="Vue logo" /> </a
+      ></span>
+      <span
+        ><a href="https://tailwindcss.org/" target="_blank">
+          <img
+            src="./assets/tailwindcss.svg"
+            class="h-16"
+            alt="TailwindCss logo"
+          /> </a
+      ></span>
+    </div>
+    <div>
+      <HelloWorld msg="Vite + Vue 3 + Tailwindcss" />
+    </div>
+  </div>
+</template>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div
+    class="
+      mx-auto
+      h-screen[100vh-10vh]
+      bg-gradient-to-r
+      from-blue-400
+      to-emerald-400
+    "
+  >
+    <div class="max-w-xl mx-auto text-center h-[60vh] p-20 py-24 space-y-8">
+      <h1 class="text-3xl font-bold underline py-6">{{ msg }}</h1>
+      <h3 class="text-bold text-2xl">Hello World</h3>
+      <div class="block">
+        <button
+          class="
+            inline-flex
+            justify-center
+            items-center
+            space-x-2
+            border
+            font-semibold
+            focus:outline-none
+            px-3
+            py-2
+            leading-6
+            rounded
+            border-cyan-200
+            bg-cyan-200
+            text-cyan-700
+            hover:text-cyan-700 hover:bg-cyan-300 hover:border-cyan-300
+            focus:ring focus:ring-cyan-500 focus:ring-opacity-50
+            active:bg-cyan-200 active:border-cyan-200
+          "
+          type="button"
+          @click="count++"
+        >
+          Count : {{ count }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
